7278e4709192d45ee3edd54cfaef0f4153962ce0
ikxneng <97749494+ikxneng@users.noreply.github.com>
2024-07-23 10:27:20 +0200

    Update script.js

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,33 +1,40 @@
 const quoteDisplay = document.getElementById('quoteDisplay');
-const newQuoteText = document.getElementById('newQuoteText').value;
-const newQuoteCategory = document.getElementById('newQuoteCategory').value;
+const newQuoteText = document.getElementById('newQuoteText');
+const newQuoteCategory = document.getElementById('newQuoteCategory');
 const quotes = [
     {text: "When you give complete editorial control of your life to Him, possible becomes the plotline", category: "Life" },
     {text: "Hope your blessings are on hold until you're aligned with your destiny, so you don't miss out on them", category: "Life"},
-    {text: "A winner is a loser that tried one more time ", category: "Motivational"},
+    {text: "A winner is a loser that tried one more time", category: "Motivational"},
     {text: "You need to remember that the world is bigger than our world and the possibilities are bigger than our experiences", category: "Motivational"},
 ];
 
-showRandomQuote =>{
-    const randomIndex = Math.floor(Math.random()* quotes.length);
+const showRandomQuote = () => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
     const quote = quotes[randomIndex];
-    quoteDisplay.innerHTML = `<p>${quote.text}</p><p><em>${quote.category}</em></p>`
-}
+    quoteDisplay.innerHTML = `<p>${quote.text}</p><p><em>${quote.category}</em></p>`;
+};
 
-// createAddQuoteForm =>{
 
-// }
+document.getElementById('newQuote').addEventListener('click', showRandomQuote);
 
-addQuote =>{
-    if (newQuoteText && newQuoteCategory){
-        quotes.push({text: newQuoteText, category: newQuoteCategory});
-        document.getElementById('newQuoteText').value = '';
-        document.getElementById('newQuoteCatefory').value = '';       
+
+showRandomQuote();
+
+
+const addQuote = () => {
+
+    const newQuoteTextValue = newQuoteText.value;
+    const newQuoteCategoryValue = newQuoteCategory.value;
+    
+    if (newQuoteTextValue && newQuoteCategoryValue) {
+        quotes.push({text: newQuoteTextValue, category: newQuoteCategoryValue});
+        // Clear the input fields
+        newQuoteText.value = '';
+        newQuoteCategory.value = '';       
         alert('Quote added successfully');
     } else {
         alert('Please enter both a quote and a category');
-    }    
-}
+    }
+};
 
-document.getElementById('newQuoteText').addEventListener('click', showRandomQuote);
-showRandomQuote();
+document.getElementById('addQuote').addEventListener('click', addQuote);
